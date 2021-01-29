3c4010863607025f7e14c93c9c8228be854e8a12
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-01-29 10:32:29 -0300

    aula 03

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
-let Modal= {
-  open(){
+const Modal = {
+  open() {
     // Abrir modal
     // Adcionar a class active ao modal
-    document.querySelector('.modal-overlay') // mandei pesquisar modal-overlay no meu HTML
-    .classList
-    .add('active') // quando ele achar, ele vai adicionar "active ao meu class"
+    document
+      .querySelector(".modal-overlay") // mandei pesquisar modal-overlay no meu HTML
+      .classList.add("active"); // quando ele achar, ele vai adicionar "active ao meu class"
   },
-  close(){
+  close() {
     // fecha o modal
     // remover a class active do modal
-    document
-    .querySelector('.modal-overlay')
-    .classList
-    .remove('active')
+    document.querySelector(".modal-overlay").classList.remove("active");
+  },
+};
+
+const transactions = [
+  {
+    id: 1,
+    description: 'Luz',
+    amount: -50000,
+    date: '23/01/2021'
+  },
+  {
+    id: 1,
+    description: 'WebSite',
+    amount: 50000,
+    date: '23/01/2021'
+  },
+  {
+    id: 1,
+    description: 'Luz',
+    amount: -50000,
+    date: '23/01/2021'
+  }
+]
+
+const Transaction = {
+  icomes() {
+    // somar as entradas
+  },
+  expense() {
+    // somas as saídas
+  },
+  total() {
+    // entradas - saídas
   }
 }
