bdecaece81115bf0dd3beca0dbe31e888019e0aa
Guilherme Augusto <augusto.gui.2208@gmail.com>
2021-01-29 16:29:33 -0300

    aula 03

--- script.js
+++ script.js
@@ -21,15 +21,21 @@ const transactions = [
     date: '23/01/2021'
   },
   {
-    id: 1,
+    id: 2,
     description: 'WebSite',
     amount: 50000,
     date: '23/01/2021'
   },
   {
-    id: 1,
+    id: 3,
     description: 'Internet',
     amount: -20000,
+    date: '23/01/2021'
+  },
+  {
+    id: 4,
+    description: 'App',
+    amount: 500000,
     date: '23/01/2021'
   }
 ]
@@ -49,23 +55,49 @@ const Transaction = {
 // Substituir os dados do HTML com os dados do JS
 
 const DOM = {
-  
-    addTransaction(transaction, index){
-      const tr = document.createElement('tr')
-      tr.innerHTML = DOM.innerHTMLTransaction()
-    },
-    innerHTMLTransaction() {
+  transactionsContainer: document.querySelector('#data-table tbody'),
+
+  addTransaction(transaction, index) {
+    const tr = document.createElement('tr')
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+    DOM.transactionsContainer.appendChild(tr)
+  },
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+    const amount = Utils.formatCurrency(transaction.amount)
+
     const html = `
-      <td class="description">Luz</td>
-      <td class="expense">- R$ 500,00</td>
-      <td class="date">23/01/2021</td>
-    <td>
-      <img src="./assets/minus.svg" alt="Remover transação" />
-    </td>
-    `
+      <td class="description">${transaction.description}</td>
+      <td class="${CSSclass}">${amount}</td>
+      <td class="date">${transaction.date}</td>
+      <td>
+        <img src="./assets/minus.svg" alt="Remover transação" />
+      </td>
+      `
 
     return html
   }
 }
 
-DOM.innerHTMLTransaction()
+const Utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? "-" : ""
+
+    value = String(value).replace(/\D/g, "")
+
+    value = Number(value) / 100
+
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    })
+    return signal + value
+  }
+}
+
+
+transactions.forEach(function (transaction) {
+  DOM.addTransaction(transaction)
+})
